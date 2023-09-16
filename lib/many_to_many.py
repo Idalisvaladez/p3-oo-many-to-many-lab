7e05ec3d8da0f0b0de5f59d294f08ad306b4cbbd
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-15 22:55:18 -0500

    Complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,89 @@
 class Author:
-    pass
+    all =  []
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        total = sum([contract.royalties for contract in self.contracts()])
+        return total
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalites):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalites
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise Exception("Author needs to be of Author type")
+
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, book):
+        if isinstance(book, Book):
+            self._book = book
+        else:
+            raise Exception("Book needs to be of Book type")
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, date):
+        if isinstance(date, str):
+            self._date = date
+        else:
+            raise Exception("Date must be of type string")
+    
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, royalties):
+        if isinstance(royalties, int):
+            self._royalties = royalties
+        else:
+            raise Exception("Royalties must be of type integer")
+
+    @classmethod  
+    def contracts_by_date(cls):
+        return sorted(cls.all, key=lambda contract: contract.date)
+    
